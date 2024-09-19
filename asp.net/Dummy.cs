11a5c781e5f2eb9ae104f6d6855941f54d30daf6
DESKTOP-QE9UGV8\redbindy <redbindy@naver.com>
2024-09-19 14:30:26 +0900

    DesignSample

---
+++ asp.net/Dummy.cs
@@ -0,0 +1,16 @@
+﻿namespace Capstone
+{
+    public class Dummy
+    {
+        public string Title { get; }
+        public string Date { get; }
+        public string Tag { get; }
+
+        public Dummy(string title, string data, string tag)
+        { 
+            Title = title;
+            Date = data;
+            Tag = tag;
+        }
+    }
+}
