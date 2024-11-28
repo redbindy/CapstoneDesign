43a8060e91734818d5ab5ac5729a33430735368e
BOOK-OJIP4S0L7K\dmagk <dmagk560@gmail.com>
2024-11-28 16:24:26 +0900

    Add PythonExecuter only execute visitor.py

--- asp.net/Database/PythonExecuter.cs
+++ asp.net/Database/PythonExecuter.cs
@@ -1,23 +1,63 @@
 ﻿using System.Diagnostics;
+using System.Text;
 
 namespace Capstone.Database
 {
-    public class PythonExecuter
+    public static class PythonExecuter
     {
-        public static void ExecuteVisitorPy()
+        private const string DB_PATH = @"wwwroot/db/";
+#if DEBUG
+        private const int TIMER_INTERVAL_MS = 1000 * 10;
+#else
+        private const int TIMER_INTERVAL_MS = 1000 * 60 * 90;
+#endif
+        private static readonly System.Timers.Timer EXECUTER_TIMER = new System.Timers.Timer();
+        private static StringBuilder visitorHTML = new StringBuilder(Program.DEFAULT_CAPACITY);
+
+        static PythonExecuter()
+        {
+            executeVisitorPy();
+
+            EXECUTER_TIMER.Interval = TIMER_INTERVAL_MS;
+            EXECUTER_TIMER.Elapsed += (sender, e) => executeVisitorPy();
+            EXECUTER_TIMER.AutoReset = true;
+
+            EXECUTER_TIMER.Start();
+        }
+
+        public static string GetVisitorHTML()
+        {
+            Debug.Assert(visitorHTML.Length != 0);
+            return visitorHTML.ToString();
+        }
+
+        private static void executeVisitorPy()
         {
             try
             {
-                const string SCRIPT_PATH = @"wwwroot/db/scripts/visitor.py";
+                ProcessStartInfo scriptInfo = new ProcessStartInfo($"{DB_PATH}/script/");
+                scriptInfo.FileName = "python.exe";
+                scriptInfo.Arguments = "visitor.py";
+                scriptInfo.UseShellExecute = false;
+                scriptInfo.RedirectStandardOutput = true;
+                scriptInfo.RedirectStandardError = true;
 
-
-                ProcessStartInfo start = new ProcessStartInfo(SCRIPT_PATH);
-                start.FileName = "python.exe";
-                start.Arguments = SCRIPT_PATH;
-                
-                using (Process process = Process.Start(start))
+                using (Process? process = Process.Start(scriptInfo))
                 {
+                    string output = process.StandardOutput.ReadLine();
+                    string error = process.StandardError.ReadToEnd();
 
+                    Console.WriteLine(output);
+                    Console.WriteLine(error);
+                }
+
+                using (StreamReader sr = new StreamReader(File.Open($"{DB_PATH}/VisitorGraph.html", FileMode.Open)))
+                {
+                    visitorHTML.Clear();
+                    while (!sr.EndOfStream)
+                    {
+                        visitorHTML.AppendLine(sr.ReadLine());
+                    }
                 }
             }
             catch (Exception e)
@@ -26,7 +66,6 @@ namespace Capstone.Database
             }
             finally
             {
-
             }
         }
     }
